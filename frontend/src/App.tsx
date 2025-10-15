9c0b7d829cecb1c5adca472fcbf9207dbf35802e
dnlsjtmznzl100g <hoon106733@yonsei.ac.kr>
2025-10-15 23:59:38 +0900

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,16 @@
+frontend/src/App.tsx
+
+import React from "react";
+import TranslateForm from "./components/TranslateForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Prompt Translator Web</h1>
+      <TranslateForm />
+    </div>
+  );
+}
+
+export default App;
 
